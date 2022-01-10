60ea0789a79c83bf9d6c9395284a49607f0a375d
dathrob <dev.misganaw.meseret@gmail.com>
2022-01-10 16:55:19 +0300

    deny direct access

---
+++ dathrobplugin.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+* Plugin Name: Dathrob's plugin
+* Plugin URI: http://github.com/dathrob
+* Description: This plugin is developed for plugin replacement assignment 
+* Version: 1.0.0
+* Author: Misganaw M.
+* License: GPL2
+*/
+
+/*exit(deny) is accesed if accessed directly*/
+if( ! defined ('ABSPATH')){
+    exit;
+}
+?>
