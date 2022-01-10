e31415ec2658f209d47352d2c23eb3db00e1ea37
dathrob <dev.misganaw.meseret@gmail.com>
2022-01-10 17:25:17 +0300

    add default social profile(facebook)

--- dathrobplugin.php
+++ dathrobplugin.php
@@ -13,4 +13,26 @@
 if( ! defined ('ABSPATH')){
     exit;
 }
+
+
+function dt_get_social_profiles(){
+
+    return apply_filters(
+        'dt_social_profiles',
+        array()
+    );
+}
+function dt_register_default_social_profiles($profiles){
+    //facebook profile
+    $profiles['facebook'] = array(
+        'id'     => 'dt_facebook_url',
+        'label'  => __('Facebook URL','dt-social'),/*translatable */
+        'class'  => 'facebook',
+        'description' => __('Enter your Facebook profile  URL','dt-social'),
+        'priority'   => 10,/*for ordering in the custumizer */
+        'type'     => '',
+        'sanitize_callback' => 'santize_text_field'
+    );
+    return $profiles;
+}
 ?>
