f189d34e50c7d640a2d84a64d8aeb86594553bc2
dathrob <dev.misganaw.meseret@gmail.com>
2022-01-18 20:19:41 +0300

    try oop on plugin dev

--- dathrobplugin.php
+++ dathrobplugin.php
@@ -2,10 +2,10 @@
 
 /**
 * Plugin Name: Dathrob's plugin
-* Plugin URI: http://github.com/dathrob
+* Plugin URI: http://github.com/dathrob1
 * Description: This plugin is developed for plugin replacement assignment 
 * Version: 1.0.0
-* Author: Misganaw M.
+* Author: Misganaw ME.
 * License: GPL2
 */
 
@@ -13,75 +13,24 @@
 if( ! defined ('ABSPATH')){
     exit;
 }
+class Dathrobplugin
+{
 
-
-function dt_get_social_profiles(){
-
-    return apply_filters(
-        'dt_social_profiles',
-        array()
-    );
-}
-function dt_register_default_social_profiles($profiles){
-    //facebook profile
-    $profiles['facebook'] = array(
-        'id'     => 'dt_facebook_url',
-        'label'  => __('Facebook URL','dt-social'),/*translatable */
-        'class'  => 'facebook',
-        'description' => __('Enter your Facebook profile  URL','dt-social'),
-        'priority'   => 10,/*for ordering in the custumizer  */
-        'type'     => '',
-        'sanitize_callback' => 'santize_text_field'
-    );
-    return $profiles;
-}
-add_filter('dt_social_profiles','dt_register_default_social_profiles',10,1);
-
-function dt_register_social_customizer_settings($wp_customize){
-
-    $social_profiles = dt_get_social_profiles();
-
-    if(! empty( $social_profiles)){
-
-        $wp_customize->add_section(
-            'dt_social',
-            array(
-                'title'  => __(' Social Profiles'),
-                'description'    => __( 'Add social media profiles here.' ),
-				'priority'       => 160,
-				'capability'     => 'edit_theme_options',
-			)
-		);
-
-		// loop through each progile.
-		foreach ( $social_profiles as $social_profile ) {
-
-			// add the customizer setting for this profile.
-			$wp_customize->add_setting(
-				$social_profile['id'],
-				array(
-					'default'           => '',
-					'sanitize_callback' => $social_profile['sanitize_callback'],
-				)
-			);
-
-			// add the customizer control for this profile.
-			$wp_customize->add_control(
-				$social_profile['id'],
-				array(
-					'type'        => $social_profile['type'],
-					'priority'    => $social_profile['priority'],
-					'section'     => 'dt_social',
-					'label'       => $social_profile['label'],
-					'description' => $social_profile['description'],
-				)
-			);
-
-		}
-
+	function __construct(){
+		add_action('init',array( $this,'dathrob_social_post_type'));
 	}
 
+
+ function dathrob_social_post_type() { 
+	 register_post_type( 'social_icon',['public' => 'true']);
+ }
+
+ 
 }
-add_action( 'customize_register', 'dt_register_social_customizer_settings' );
+if( class_exists('Dathrobplugin')){
+	$dathrobplugin = new Dathrobplugin();
+}
+
+
  
 ?> 
