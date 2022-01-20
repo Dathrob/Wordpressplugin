9bfd4bb386a0418a5822c63cf7a567652d23e9a7
dathrob <dev.misganaw.meseret@gmail.com>
2022-01-20 13:49:17 +0300

    modularized templates

--- dathrobplugin.php
+++ dathrobplugin.php
@@ -30,102 +30,18 @@ class Dathrobplugin
 	 add_menu_page('dathrobsocial','dathrob social','manage_options','dathrob_menu',array($this,'dathrob_main'),'dashicons-share-alt2',100);
 }
   
+ /*function dathrob_main(){
+	return array($this,'option1');
+ } */
  function dathrob_main(){
-	echo'
-	<style>  
-	body {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		height: 500vh;
-		width: 50vw;
-		margin: -8px;
-		background: #e0e5ec;
-	  }
-	  
-	  
-	  .frame{
-		display: flex;
-		flex-direction: row;
-		justify-content: space-around;
-		align-items: center;
-		margin: 20px;
-		height: 80px;
-		width: 350px;
-		position: relative;
-		 box-shadow:
-		 -7px -7px 20px 0px #fff9,
-		 -4px -4px 5px 0px #fff9,
-		 7px 7px 20px 0px #0002,
-		 4px 4px 5px 0px #0001,
-		 inset 0px 0px 0px 0px #fff9,
-		 inset 0px 0px 0px 0px #0001,
-		 inset 0px 0px 0px 0px #fff9,        inset 0px 0px 0px 0px #0001;
-	   transition:box-shadow 0.6s cubic-bezier(.79,.21,.06,.81);
-		 border-radius: 10px;
-	  }
-	  
-	  .btn{
-		height: 35px;
-		width: 35px;
-		border-radius: 3px;
-		background: #e0e5ec;
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		-webkit-tap-highlight-color: rgba(0,0,0,0);
-		-webkit-tap-highlight-color: transparent;
-		box-shadow:
-		 -7px -7px 20px 0px #fff9,
-		 -4px -4px 5px 0px #fff9,
-		 7px 7px 20px 0px #0002,
-		 4px 4px 5px 0px #0001,
-		 inset 0px 0px 0px 0px #fff9,
-		 inset 0px 0px 0px 0px #0001,
-		 inset 0px 0px 0px 0px #fff9,        inset 0px 0px 0px 0px #0001;
-	   transition:box-shadow 0.6s cubic-bezier(.79,.21,.06,.81);
-		font-size: 16px;
-		color: rgba(42, 52, 84, 1);
-		text-decoration: none;
-	  }
-	  .btn:active{
-		box-shadow:  4px 4px 6px 0 rgba(255,255,255,.5),
-					-4px -4px 6px 0 rgba(116, 125, 136, .2), 
-		  inset -4px -4px 6px 0 rgba(255,255,255,.5),
-		  inset 4px 4px 6px 0 rgba(116, 125, 136, .3);
-	  }
-	</style>
+	include_once plugin_dir_path(__FILE__) .'includes/option1.php';
+	wp_enqueue_style( 'option1css', plugin_dir_url(__FILE__) .'assets/css/option1.css');
+	include_once plugin_dir_path(__FILE__) .'includes/option2.php';
+	wp_enqueue_style( 'option2css', plugin_dir_url(__FILE__) .'assets/css/option2.css');
+	echo '
+	<h1>this is from the function </h1>';
+}
 
-	<head>
-<link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
-</head>
-
-<body>
-  <h1>option 1</h1>
-  <div class ="frame">
-    <a href="#" class="btn">
- <i class="fab fa-facebook-f" style="color: #3b5998;"></i>
-</a>
-    <a href="#" class="btn">
-  <i class="fab fa-twitter" style="color: #00acee;"></i>
-</a>
-    <a href="#" class="btn">
- <i class="fab fa-dribbble" style="color: #ea4c89;"></i>
-</a>
-    <a href="#" class="btn">
- <i class="fab fa-linkedin-in" style="color:#0e76a8;"></i>
-</a>
-    <a href="#" class="btn">
- <i class="fab fa-get-pocket" style="color:#ee4056;"></i>
-</a>
-    <a href="#" class="btn">
- <i class="far fa-envelope"></i>
-</a>
-</div>
-</body>';
- } 
  
 }
 if( class_exists('Dathrobplugin')){
---
+++ includes/option2.php
@@ -0,0 +1,15 @@
+<h1>option2<h1>
+<link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+<br/>
+<div align="center" class="socialbtns">
+<ul>
+<li><a href="#" class="fa fa-lg fa-facebook"></a></li>
+<li><a href="#" class="fa fa-lg fa-twitter"></a></li>
+<li><a href="#" class="fa fa-lg fa-google-plus"></a></li>
+<li><a href="#" class="fa fa-lg fa-github"></a></li>
+<li><a href="#" class="fa fa-lg fa-pinterest"></a></li>
+<li><a href="#" class="fa fa-lg fa-linkedin"></a></li>
+<li><a href="#" class="fa fa-lg fa-instagram"></a></li>
+<li><a href="#" class="fa fa-lg fa-youtube"></a></li>
+</ul>
+</div>
