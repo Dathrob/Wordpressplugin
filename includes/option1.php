b661a05f7da1d4b45f405f986f56b0ec0c912ee5
dathrob <dev.misganaw.meseret@gmail.com>
2022-01-25 22:47:39 +0300

    fix placement issue of sidebar social icons

--- includes/option1.php
+++ includes/option1.php
@@ -1,10 +1,10 @@
 <div class="stickysidebar">
     <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
     <ul class="dsocial-media">
-        <li><a  href="#" class="dicon"><i class="fab fa-facebook fa-3x"></i></a></li>
-        <li><a href="#" class="dicon"><i class="fab fa-twitter fa-3x"></i></a></li>
-        <li><a href="#" class="dicon"><i class="fab fa-instagram fa-3x"></i></a></li>
-        <li><a href="#" class="dicon"><i class="fab fa-google fa-3x"></i></a></li>
-        <li><a href="#" class="dicon"><i class="fab fa-youtube fa-3x"></i></a></li>
+        <li><a  href="#" class="dicon"><i class="fab fa-facebook fa-2x"></i></a></li>
+        <li><a href="#" class="dicon"><i class="fab fa-twitter fa-2x"></i></a></li>
+        <li><a href="#" class="dicon"><i class="fab fa-instagram fa-2x"></i></a></li>
+        <li><a href="#" class="dicon"><i class="fab fa-google fa-2x"></i></a></li>
+        <li><a href="#" class="dicon"><i class="fab fa-youtube fa-2x"></i></a></li>
       </ul>
 </div>
